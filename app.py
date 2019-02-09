7fb0731ed7b38b1bdf3aad7feae9c0c83bbadaed
GreenSpanA <vladimir.davidko@phystech.edu>
2019-02-09 22:28:49 +0300

    Add posobility to download file

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
-from flask import Flask
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+
+UPLOAD_FOLDER = 'UPLOAD_FOLDER/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+base_path = os.path.abspath(os.path.dirname(__file__))
+upload_path = os.path.join(base_path, app.config['UPLOAD_FOLDER'])
+app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-	return 'Hello World!'
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(upload_path, filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(upload_path,
+                               filename)
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',  port=80)
+	app.run(host='0.0.0.0',  port=8000)
